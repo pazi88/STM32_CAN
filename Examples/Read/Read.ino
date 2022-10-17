cf1828250b48220807ca501f29a99041cba37589
Pasi Kemppainen <48950874+pazi88@users.noreply.github.com>
2022-10-17 13:39:59 +0300

    Improve examples

--- Examples/Read/Read.ino
+++ Examples/Read/Read.ino
@@ -1,18 +1,28 @@
+/*
+This is simple example to read all data from CAN bus and print it out to serial bus.
+*/
+
 #include "STM32_CAN.h"
-//STM32_CAN Can1( CAN1, DEF );
-//STM32_CAN Can1( CAN1, ALT );
-STM32_CAN Can1( CAN1, ALT_2 );
+STM32_CAN Can( CAN1, DEF );  //Use PA11/12 pins for CAN1.
+//STM32_CAN Can( CAN1, ALT );  //Use PB8/9 pins for CAN1.
+//STM32_CAN Can( CAN1, ALT_2 );  //Use PD0/1 pins for CAN1.
+//STM32_CAN Can( CAN2, DEF );  //Use PB12/13 pins for CAN2.
+//STM32_CAN Can( CAN2, ALT );  //Use PB5/6 pins for CAN2
+//STM32_CAN Can( CAN3, DEF );  //Use PA8/15 pins for CAN3.
+//STM32_CAN Can( CAN3, ALT );  //Use PB3/4 pins for CAN3
 
 static CAN_message_t CAN_RX_msg;
 
 void setup() {
   Serial.begin(115200);
-  Can1.begin();
-  Can1.setBaudRate(500000);
+  Can.begin();
+  //Can.setBaudRate(250000);  //250KBPS
+  Can.setBaudRate(500000);  //500KBPS
+  //Can.setBaudRate(1000000);  //1000KBPS
 }
 
 void loop() {
-  if (Can1.read(CAN_RX_msg) ) {
+  if (Can.read(CAN_RX_msg) ) {
     Serial.print("Channel:");
     Serial.print(CAN_RX_msg.bus);
     Serial.print(" Standard ID:");
--- Examples/Write/Write.ino
+++ Examples/Write/Write.ino
@@ -1,92 +1,96 @@
+/*
+This is simple example to send random data to CAN bus in 20Hz rate, using delay (not recommended in real implementations).
+*/
+
 #include "STM32_CAN.h"
-STM32_CAN Can1( CAN1, DEF );
-//STM32_CAN Can1( CAN1, ALT );
+STM32_CAN Can( CAN1, DEF );  //Use PA11/12 pins for CAN1.
+//STM32_CAN Can( CAN1, ALT );  //Use PB8/9 pins for CAN1.
+//STM32_CAN Can( CAN1, ALT_2 );  //Use PD0/1 pins for CAN1.
+//STM32_CAN Can( CAN2, DEF );  //Use PB12/13 pins for CAN2.
+//STM32_CAN Can( CAN2, ALT );  //Use PB5/6 pins for CAN2
+//STM32_CAN Can( CAN3, DEF );  //Use PA8/15 pins for CAN3.
+//STM32_CAN Can( CAN3, ALT );  //Use PB3/4 pins for CAN3
+
+static CAN_message_t CAN_TX_msg;
 
 void setup() {
   Serial.begin(115200);
-  delay(1000);
-
+  Can.begin();
+  //Can.setBaudRate(250000);  //250KBPS
+  Can.setBaudRate(500000);  //500KBPS
+  //Can.setBaudRate(1000000);  //1000KBPS
 }
 
 void loop() {
-  uint8_t recv_ch = 1;
-  static CAN_message_t CAN_TX_msg;
-
-  Can1.begin();
-  Can1.setBaudRate(500000);
-  uint8_t SecL = 0;
+  uint8_t Counter = 0;
   while(1){
-    if (SecL > 255){ SecL = 0;}
+    if (Counter > 255){ Counter = 0;}
     delay(50);
     CAN_TX_msg.id = (0x1A5);
     CAN_TX_msg.len = 8;
-    CAN_TX_msg.buf[0] =  0x03;                    // sending 3 bytes
-    CAN_TX_msg.buf[1] =  0x41;                    // Same as query, except that 40h is added to the mode value. So:41h = show current data ,42h = freeze frame ,etc.
-    CAN_TX_msg.buf[2] =  0x11;                    // pid code
-    CAN_TX_msg.buf[3] =  SecL;                    // A
-    CAN_TX_msg.buf[4] =  0x00;                    // B
-    CAN_TX_msg.buf[5] =  0x00; 
-    CAN_TX_msg.buf[6] =  0x00; 
+    CAN_TX_msg.buf[0] =  0x03;
+    CAN_TX_msg.buf[1] =  0x41;
+    CAN_TX_msg.buf[2] =  0x11;
+    CAN_TX_msg.buf[3] =  Counter;
+    CAN_TX_msg.buf[4] =  0x00;
+    CAN_TX_msg.buf[5] =  0x00;
+    CAN_TX_msg.buf[6] =  0x00;
     CAN_TX_msg.buf[7] =  0x00;
   
-    Can1.write(CAN_TX_msg);
+    Can.write(CAN_TX_msg);
 
     CAN_TX_msg.id = (0x7E8);
     CAN_TX_msg.len = 8;
-    CAN_TX_msg.buf[0] =  0x03;                    // sending 3 bytes
-    CAN_TX_msg.buf[1] =  0x41;                    // Same as query, except that 40h is added to the mode value. So:41h = show current data ,42h = freeze frame ,etc.
-    CAN_TX_msg.buf[2] =  0x11;                    // pid code
-    CAN_TX_msg.buf[3] =  0x21;                    // A
-    CAN_TX_msg.buf[4] =  0x00;                    // B
-    CAN_TX_msg.buf[5] =  SecL; 
-    CAN_TX_msg.buf[6] =  0x00; 
-    CAN_TX_msg.buf[7] =  0x00;
-  
-    Can1.write(CAN_TX_msg);
+    CAN_TX_msg.buf[0] =  0x03;
+    CAN_TX_msg.buf[1] =  0x41;
+    CAN_TX_msg.buf[3] =  0x21;
+    CAN_TX_msg.buf[4] =  0x00;
+    CAN_TX_msg.buf[5] =  Counter;
+    CAN_TX_msg.buf[6] =  0x00;
+    CAN_TX_msg.buf[7] =  0xFF;
+
+    Can.write(CAN_TX_msg);
 
     CAN_TX_msg.id = (0xA63);
     CAN_TX_msg.len = 8;
-    CAN_TX_msg.buf[0] =  0x03;                    // sending 3 bytes
-    CAN_TX_msg.buf[1] =  0x41;                    // Same as query, except that 40h is added to the mode value. So:41h = show current data ,42h = freeze frame ,etc.
-    CAN_TX_msg.buf[2] =  0x11;                    // pid code
-    CAN_TX_msg.buf[3] =  0x33;                    // A
-    CAN_TX_msg.buf[4] =  0x00;                    // B
-    CAN_TX_msg.buf[5] =  0x00; 
-    CAN_TX_msg.buf[6] =  0x00; 
-    CAN_TX_msg.buf[7] =  SecL;
+    CAN_TX_msg.buf[0] =  0x63;
+    CAN_TX_msg.buf[1] =  0x49;
+    CAN_TX_msg.buf[2] =  0x11;
+    CAN_TX_msg.buf[3] =  0x22;
+    CAN_TX_msg.buf[4] =  0x00;
+    CAN_TX_msg.buf[5] =  0x00;
+    CAN_TX_msg.buf[6] =  Counter;
+    CAN_TX_msg.buf[7] =  0x00;
 
-  
-    Can1.write(CAN_TX_msg);
+    Can.write(CAN_TX_msg);
 
     CAN_TX_msg.id = (0x23);
     CAN_TX_msg.len = 8;
-    CAN_TX_msg.buf[0] =  0x03;                    // sending 3 bytes
-    CAN_TX_msg.buf[1] =  0x41;                    // Same as query, except that 40h is added to the mode value. So:41h = show current data ,42h = freeze frame ,etc.
-    CAN_TX_msg.buf[2] =  0x11;                    // pid code
-    CAN_TX_msg.buf[3] =  0x33;                    // A
-    CAN_TX_msg.buf[4] =  0x00;                    // B
-    CAN_TX_msg.buf[5] =  0x00; 
-    CAN_TX_msg.buf[6] =  0x00; 
-    CAN_TX_msg.buf[7] =  SecL;
+    CAN_TX_msg.buf[0] =  0x03;
+    CAN_TX_msg.buf[1] =  0x41;
+    CAN_TX_msg.buf[2] =  0x11;
+    CAN_TX_msg.buf[3] =  0x33;
+    CAN_TX_msg.buf[4] =  0x00;
+    CAN_TX_msg.buf[5] =  0x00;
+    CAN_TX_msg.buf[6] =  0x00;
+    CAN_TX_msg.buf[7] =  Counter;
 
-  
-    Can1.write(CAN_TX_msg);
+    Can.write(CAN_TX_msg);
 
     CAN_TX_msg.id = (0x55);
     CAN_TX_msg.len = 8;
-    CAN_TX_msg.buf[0] =  0x03;                    // sending 3 bytes
-    CAN_TX_msg.buf[1] =  0x41;                    // Same as query, except that 40h is added to the mode value. So:41h = show current data ,42h = freeze frame ,etc.
-    CAN_TX_msg.buf[2] =  0x11;                    // pid code
-    CAN_TX_msg.buf[3] =  0x33;                    // A
-    CAN_TX_msg.buf[4] =  0x00;                    // B
-    CAN_TX_msg.buf[5] =  0x00; 
-    CAN_TX_msg.buf[6] =  0x00; 
-    CAN_TX_msg.buf[7] =  SecL;
+    CAN_TX_msg.buf[0] =  0x03;
+    CAN_TX_msg.buf[1] =  0x44;
+    CAN_TX_msg.buf[2] =  0x31;
+    CAN_TX_msg.buf[3] =  0x53;
+    CAN_TX_msg.buf[4] =  0x00;
+    CAN_TX_msg.buf[5] =  Counter;
+    CAN_TX_msg.buf[6] =  0x00;
+    CAN_TX_msg.buf[7] =  0x00;
 
-  
-    Can1.write(CAN_TX_msg);
+    Can.write(CAN_TX_msg);
     Serial.print("Sent: ");
-    Serial.println(SecL, HEX);
-    SecL++;
+    Serial.println(Counter, HEX);
+    Counter++;
   }
 }
