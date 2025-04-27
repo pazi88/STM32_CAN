eedb496f0bd733b4815914c1350541f45a8a8cc1
duckylotl <163729266+duckylotl@users.noreply.github.com>
2025-04-27 11:04:10 +0200

    fix: enable F1 alternate function mapping for CAN

--- examples/Read-Write_with_filters/hal_conf_extra.h
+++ examples/Read-Write_with_filters/hal_conf_extra.h
@@ -1,6 +1,11 @@
 #if !defined(HAL_CAN_MODULE_ENABLED)
     #define HAL_CAN_MODULE_ENABLED
 
+    #if defined(STM32F1xx)
+        /** NOTE: Enable special alternate function remapping for F1 platform*/
+        #define AFIO_MAPR_CAN_REMAP1
+    #endif
+
     #if defined(USBCON) && defined(STM32F1xx)
         /** NOTE: On the F1 platform CAN and USB may not be used at the same time.
          * To still allow a program to be build that may use both
